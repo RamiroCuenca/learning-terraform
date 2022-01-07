3b0e9fb6638632e729926db11a3a892ed2f4d6a5
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2022-01-07 09:04:47 -0300

    Created terraform cloud backend

--- 000_getting_started/providers.tf
+++ 000_getting_started/providers.tf
@@ -1,13 +1,3 @@
-# Search for a provider at https://registry.terraform.io/
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.70.0"
-    }
-  }
-}
-
 # Config an AWS profile for terraform -> aws config --profile terraform
 provider "aws" {
   profile = "terraform"
